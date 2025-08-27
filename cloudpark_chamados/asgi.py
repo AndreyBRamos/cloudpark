e396c6d2c59d0e130c84813ecb88042e65a4a89d
andreybr <andreybramos@hotmail.com>
2025-08-27 19:46:49 -0300

    chore: projeto django + app chamados + deps

---
+++ cloudpark_chamados/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cloudpark_chamados project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloudpark_chamados.settings')
+
+application = get_asgi_application()
