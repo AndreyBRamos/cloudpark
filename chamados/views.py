f5f0c675c87bbb59026e91b460f9e872e50e6ac9
andreybr <andreybramos@hotmail.com>
2025-08-27 23:41:33 -0300

    feat(ui-chamados):  alterado campo "atribuido a" de numerido para um dropdown com email dos usuarios cadastrados

--- chamados/views.py
+++ chamados/views.py
@@ -5,7 +5,8 @@ from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Ticket
-from .serializers import TicketSerializer
+from .serializers import TicketSerializer, UserMiniSerializer
+from django.contrib.auth.models import User
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
@@ -31,3 +32,10 @@ class TicketViewSet(viewsets.ModelViewSet):
             return self.get_paginated_response(ser.data)
         ser = self.get_serializer(qs, many=True)
         return Response(ser.data)
+
+    @action(detail=False, methods=["get"])
+    def usuarios(self, request):
+        """Lista usuários disponíveis para atribuição de tickets"""
+        usuarios = User.objects.filter(is_active=True).order_by('username')
+        serializer = UserMiniSerializer(usuarios, many=True)
+        return Response(serializer.data)
